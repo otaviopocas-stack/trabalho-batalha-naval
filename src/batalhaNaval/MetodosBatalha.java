5cd78c840dac1bf13941af56e4a08fb801014769
Gustavo Correia Machado <gu.vip.112@gmail.com>
2026-06-21 18:21:15 -0300

    Adicionado posição de NAVIO Horizontal, Vertical, Diagonal Principal e Secundaria, verificação de posição no tabuleiro e Posicionar Navios.

--- src/batalhaNaval/MetodosBatalha.java
+++ src/batalhaNaval/MetodosBatalha.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MetodosBatalha {
 
     public static int tamanho = 10;
@@ -5,6 +7,7 @@ public class MetodosBatalha {
     public static char navio = 'N';
     public static char acerto = 'X';
     public static char erro = 'O';
+    public static Scanner leitura = new Scanner(System.in);
 
     public static char[][] inicializarTabuleiro() {
         char[][] tabuleiroInicial = new char[tamanho][tamanho];
@@ -21,12 +24,20 @@ public class MetodosBatalha {
     public static void exibirInstrucoesDefensor() {
         System.out.println("-- Instruções do Defensor --");
         System.out.println("- Deve-se usar as coordenadas para posicionar seus navios;");
+        System.out.println("- Informe linha e coluna inicial do navio;");
+        System.out.println("- Escolha uma direção para o navio:");
+        System.out.println("  H = Horizontal --");
+        System.out.println("  V = Vertical |");
+        System.out.println("  D = Diagonal Principal");
+        System.out.println("  S = Diagonal Secundária /");
+        System.out.println();
         System.out.println("Você possui os seguintes navios:");
         System.out.println("- 1 Porta-Aviões de tamanho 4;");
         System.out.println("- 1 Fragata de tamanho 3;");
         System.out.println("- 3 Submarinos de tamanho 2;");
         System.out.println("- 3 Botes de tamanho 1;");
-        System.out.println("- Os navios podem ser posicionados na vertical, horizontal ou diagonal.");
+        System.out.println("- Os navios podem ser posicionados na vertical, horizontal, diagonal principal ou diagonal secundária.");
+        System.out.println("-Só é escolhida a posição inicial, após vai ser somada sempre 1 posição dependendo do tamanho do Navio");
     }
 
     public static void exibirTabuleiro(char[][] tabuleiro, String titulo) {
@@ -49,27 +60,161 @@ public class MetodosBatalha {
         }
     }
 
-    public static boolean posicionarNavio(char[][] tabuleiro, int linha, int coluna, int tamanhoNavio) {
-        // Verifica se o navio cabe na horizontal a partir da posição fornecida
-        if (coluna + tamanhoNavio > tamanho) {
-            System.out.println("Posição inválida! O navio não cabe na horizontal.");
+    public static boolean posicionarNavio(char[][] tabuleiro, int linha, int coluna, int tamanhoNavio, char direcao) {
+
+    // Verificação das posições
+    for (int i = 0; i < tamanhoNavio; i++) {
+
+        int novaLinha = linha;
+        int novaColuna = coluna;
+
+        if (direcao == 'H' || direcao == 'h') {
+            novaColuna = coluna + i;
+        }
+        else if (direcao == 'V' || direcao == 'v') {
+            novaLinha = linha + i;
+        }
+        else if (direcao == 'D' || direcao == 'd') {
+            novaLinha = linha + i;
+            novaColuna = coluna + i;
+        }
+        else if (direcao == 'S' || direcao == 's') {
+            novaLinha = linha + i;
+            novaColuna = coluna - i;
+        }
+        else {
+            System.out.println("Direção inválida.");
             return false;
         }
 
-        // Verifica se a posição está livre para o navio
-        for (int i = 0; i < tamanhoNavio; i++) {
-            if (tabuleiro[linha][coluna + i] != agua) {
-                System.out.println("Posição inválida! Já existe um navio aqui.");
-                return false;
-            }
+        // Verificar se saiu do tabuleiro
+        if (novaLinha >= tamanho || novaColuna >= tamanho || novaColuna < 0 || novaLinha < 0) {
+            System.out.println("O navio ultrapassa o limite do tabuleiro.");
+            return false;
         }
 
-        // Aprovado, posiciona o navio no tabuleiro
-        for (int i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linha][coluna + i] = navio; 
+        // Verificar sobreposição
+        if (tabuleiro[novaLinha][novaColuna] != agua) {
+            System.out.println("Já existe um navio nessa posição.");
+            return false;
         }
+    }
 
-        System.out.println("Navio posicionado com sucesso!");
-        return true;
+    // Posiciona o navio
+    for (int i = 0; i < tamanhoNavio; i++) {
+
+        int novaLinha = linha;
+        int novaColuna = coluna;
+
+    if (direcao == 'H' || direcao == 'h') {
+        novaColuna = coluna + i;
+    }
+    else if (direcao == 'V' || direcao == 'v') {
+        novaLinha = linha + i;
+    }
+    else if (direcao == 'D' || direcao == 'd') {
+        novaLinha = linha + i;
+        novaColuna = coluna + i;
+    }
+    else if (direcao == 'S' || direcao == 's') {
+        novaLinha = linha + i;
+        novaColuna = coluna - i;
+    }
+
+        tabuleiro[novaLinha][novaColuna] = navio;
+    }
+
+    System.out.println("Navio posicionado com sucesso!");
+    return true;
+    }
+
+    public static void posicionarPortaAvioes(char[][] tabuleiro) {
+        boolean resultado = false;
+
+        while (resultado == false) {
+            System.out.println("\nPosicionando Porta-Aviões (Tamanho 4)");
+
+            System.out.print("Linha: ");
+            int linha = leitura.nextInt();
+
+            System.out.print("Coluna: ");
+            int coluna = leitura.nextInt();
+
+            System.out.print("Direção (H/V/D/S): ");
+            char direcao = leitura.next().charAt(0);
+
+            resultado = posicionarNavio(tabuleiro, linha, coluna, 4, direcao);
+            atualizarTabuleiroDefensor(tabuleiro, resultado);
+        }
+    }
+
+    public static void posicionarFragata(char[][] tabuleiro) {
+        boolean resultado = false;
+
+        while (resultado == false) {
+
+            System.out.println("\nPosicionando Fragata (Tamanho 3)");
+
+            System.out.print("Linha: ");
+            int linha = leitura.nextInt();
+
+            System.out.print("Coluna: ");
+            int coluna = leitura.nextInt();
+
+            System.out.print("Direção (H/V/D/S): ");
+            char direcao = leitura.next().charAt(0);
+
+            resultado = posicionarNavio(tabuleiro, linha, coluna, 3, direcao);
+            atualizarTabuleiroDefensor(tabuleiro, resultado);
     }
 }
+
+    public static void posicionarSubmarino(char[][] tabuleiro, int numero) {
+        boolean resultado = false;
+
+        while (resultado == false) {
+
+            System.out.println("\nPosicionando Submarino " + numero + " (Tamanho 2)");
+
+            System.out.print("Linha: ");
+            int linha = leitura.nextInt();
+
+            System.out.print("Coluna: ");
+            int coluna = leitura.nextInt();
+
+            System.out.print("Direção (H/V/D/S): ");
+            char direcao = leitura.next().charAt(0);
+
+            resultado = posicionarNavio(tabuleiro, linha, coluna, 2, direcao);
+            atualizarTabuleiroDefensor(tabuleiro, resultado);
+    }
+}
+
+    public static void posicionarBote(char[][] tabuleiro, int numero) {
+        boolean resultado = false;
+
+        while (resultado == false) {
+            System.out.println("\nPosicionando Bote " + numero + " (Tamanho 1)");
+
+            System.out.print("Linha: ");
+            int linha = leitura.nextInt();
+
+            System.out.print("Coluna: ");
+            int coluna = leitura.nextInt();
+
+            System.out.print("Direção (H/V/D/S): ");
+            char direcao = leitura.next().charAt(0);
+
+            resultado = posicionarNavio(tabuleiro, linha, coluna, 1, direcao);
+            atualizarTabuleiroDefensor(tabuleiro, resultado);
+        }
+    }
+
+    public static void atualizarTabuleiroDefensor(char[][] tabuleiro, boolean resultado) {
+        if (resultado == true) {
+            exibirTabuleiro(tabuleiro, "Tabuleiro do Defensor");
+        }
+    }  
+
+}
+    
--- src/batalhaNaval/batalhaNaval.java
+++ src/batalhaNaval/batalhaNaval.java
@@ -1,12 +1,18 @@
-public class batalhaNaval {
+public class batalhaNaval { 
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    char[][] tabuleiroDefensor = MetodosBatalha.inicializarTabuleiro();
 
-        char[][] tabuleiroInicial = MetodosBatalha.inicializarTabuleiro();
-        String titulo = "Tabuleiro Inicial";
+    MetodosBatalha.exibirInstrucoesDefensor();
+    MetodosBatalha.posicionarPortaAvioes(tabuleiroDefensor);
+    MetodosBatalha.posicionarFragata(tabuleiroDefensor);
+    MetodosBatalha.posicionarSubmarino(tabuleiroDefensor, 1);
+    MetodosBatalha.posicionarSubmarino(tabuleiroDefensor, 2);
+    MetodosBatalha.posicionarSubmarino(tabuleiroDefensor, 3);
+    MetodosBatalha.posicionarBote(tabuleiroDefensor, 1);
+    MetodosBatalha.posicionarBote(tabuleiroDefensor, 2);
+    MetodosBatalha.posicionarBote(tabuleiroDefensor, 3);
 
-        MetodosBatalha.exibirInstrucoesDefensor();
-        MetodosBatalha.exibirTabuleiro(tabuleiroInicial, titulo);
-
+    System.out.println("\nTodos os navios foram posicionados!");
     }
 }
